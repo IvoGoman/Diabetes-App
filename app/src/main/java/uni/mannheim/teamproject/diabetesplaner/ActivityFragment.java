f9960b38925db45c3592cf338942edcfac9494a1
nayera-muhammad <nayera.muhammad@gmail.com>
2016-01-28 01:07:21 +0100

    commiting branch

---
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/ActivityFragment.java
@@ -0,0 +1,23 @@
+package uni.mannheim.teamproject.diabetesplaner;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * created by Naira
+ */
+public class ActivityFragment extends Fragment {
+
+
+    public ActivityFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
---
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/MeasurementFragment.java
@@ -0,0 +1,22 @@
+package uni.mannheim.teamproject.diabetesplaner;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * created by Naira
+ */
+
+public class MeasurementFragment extends Fragment {
+
+
+    public MeasurementFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
