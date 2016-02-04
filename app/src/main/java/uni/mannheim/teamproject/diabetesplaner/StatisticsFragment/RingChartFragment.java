f006f57e8924562b48712c3d891c71094a64d1f9
Ivo Gosemann <Ivo.Gosemann@gmail.com>
2016-02-04 16:49:32 +0100

    Implemented a first Draft Version of a Donut Chart and a Combined Line and Bar Chart  ! values are hard-coded  ! coloring and formating are not adjusted

--- app/src/main/java/uni/mannheim/teamproject/diabetesplaner/StatisticsFragment/RingChartFragment.java
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/StatisticsFragment/RingChartFragment.java
@@ -6,6 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import org.jfree.chart.entity.PieSectionEntity;
+
+import java.util.ArrayList;
+
 import uni.mannheim.teamproject.diabetesplaner.R;
 
 /**
@@ -15,6 +24,7 @@ public class RingChartFragment extends Fragment{
 
     public RingChartFragment() {
         // Required empty public constructor
+
     }
 
     @Override
@@ -26,7 +36,31 @@ public class RingChartFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ring_chart, container, false);
+        View inflaterView = inflater.inflate(R.layout.fragment_ring_chart, container, false);
+
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Running");
+        labels.add("Sleeping");
+        labels.add("Working");
+        labels.add("Eating");
+        labels.add("Relaxing");
+
+        ArrayList<Entry> pieValues = new ArrayList<>();
+        pieValues.add(new Entry(1f, 0));
+        pieValues.add(new Entry(8f, 1));
+        pieValues.add(new Entry(8f, 2));
+        pieValues.add(new Entry(3f, 3));
+        pieValues.add(new Entry(4f, 4));
+
+        PieDataSet pieDataSet = new PieDataSet(pieValues, "Activities");
+        PieData pieData = new PieData(labels, pieDataSet);
+
+        PieChart chart = (PieChart) inflaterView.findViewById(R.id.activitypiechart);
+        chart.setData(pieData);
+        chart.setUsePercentValues(true);
+        chart.setDescription("Overview of activities");
+
+        return inflaterView;
     }
 
 }
