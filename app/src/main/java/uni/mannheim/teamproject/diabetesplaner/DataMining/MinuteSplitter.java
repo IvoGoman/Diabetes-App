2faf52ba0fc2c292ae4bb69302fe8934eb2fd97f
leonidgunko <leonidgunko1@yandex.ru>
2016-02-16 12:27:44 +0100

    backend

--- app/src/main/java/uni/mannheim/teamproject/diabetesplaner/DataMining/MinuteSplitter.java
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/DataMining/MinuteSplitter.java
@@ -55,6 +55,7 @@ public class MinuteSplitter {
 					}
 				}
 				listFinal.add(Util.toArray(tmp));
+
 			}else{
 				Timestamp starttime = new Timestamp(Long.parseLong(list.get(i)[starttimeIndex]));
 				Timestamp endtime = new Timestamp(Long.parseLong(list.get(i)[endtimeIndex]));
