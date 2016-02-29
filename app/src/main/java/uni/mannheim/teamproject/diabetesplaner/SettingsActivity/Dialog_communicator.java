e4b4ad7e973eef2833a9e3fc68462d9f944d1ba1
Jan Weisenstein <Janweisenstein9270@gmail.com>
2016-02-29 10:51:04 +0100

    Added dialog communicator

---
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/SettingsActivity/Dialog_communicator.java
@@ -0,0 +1,8 @@
+package uni.mannheim.teamproject.diabetesplaner.SettingsActivity;
+
+/**
+ * Created by Jan on 26.02.16.
+ */
+public interface Dialog_communicator {
+    public void respond(String data);
+}
