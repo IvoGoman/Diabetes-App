c01009fc51086491ef507df35b7056f7dbc55999
Stefan <bassierer@gmx.de>
2016-09-14 12:10:00 +0200

    Added subpreference to select algorithm

--- app/src/main/java/uni/mannheim/teamproject/diabetesplaner/Domain/DailyRoutineHandler.java
+++ app/src/main/java/uni/mannheim/teamproject/diabetesplaner/Domain/DailyRoutineHandler.java
@@ -1,6 +1,8 @@
 package uni.mannheim.teamproject.diabetesplaner.Domain;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -30,15 +32,48 @@ public class DailyRoutineHandler extends DayHandler{
 
     /**
      * predicts the daily routine and lets you choose between different algorithms
-     * @param algorithm <li>PredictionFramework.PREDICTION_DECISION_TREE</li>
+     * @param algorithms a list with the algorithms to use. Possible algorithms:
+     *                  <li>PredictionFramework.PREDICTION_DECISION_TREE</li>
      *                  <li>PredictionFramework.PREDICTION_GSP</li>
      *                  <li>PredictionFramework.PREDICTION_FUZZY_MINER</li>
      *                  <li>PredictionFramework.PREDICTION_HEURISTICS_MINER</li>
      * @param mode     EVERY_DAY, WEEKDAYS, WEEKENDS, MONDAY, ... , SUNDAY
      * @author Stefan 06.09.2016
      */
-    public void predictDailyRoutine(int algorithm, int mode){
-        dailyRoutine = PredictionFramework.predict(PredictionFramework.retrieveTrainingData(mode), PredictionFramework.PREDICTION_GSP);
+    public void predictDailyRoutine(ArrayList<Integer> algorithms, int mode){
+        dailyRoutine = PredictionFramework.predict(PredictionFramework.retrieveTrainingData(mode), algorithms);
+    }
+
+    /**
+     * predicts the daily routine and gets the algorithms to choose from the settings
+     * @param mode     EVERY_DAY, WEEKDAYS, WEEKENDS, MONDAY, ... , SUNDAY
+     * @author Stefan 06.09.2016
+     */
+    public void predictDailyRoutine(int mode, Context context){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        ArrayList<Integer> algorithms = new ArrayList<>();
+        boolean dt = preferences.getBoolean("pref_key_dt", true);
+        if(dt){
+            algorithms.add(PredictionFramework.PREDICTION_DECISION_TREE);
+        }
+        boolean gsp = preferences.getBoolean("pref_key_gsp", true);
+        if(gsp){
+            algorithms.add(PredictionFramework.PREDICTION_GSP);
+        }
+        boolean fuzzy = preferences.getBoolean("pref_key_fuzzy", true);
+        if(fuzzy){
+            algorithms.add(PredictionFramework.PREDICTION_FUZZY_MINER);
+        }
+        boolean heuristics = preferences.getBoolean("pref_key_heuristics", true);
+        if(heuristics){
+            algorithms.add(PredictionFramework.PREDICTION_HEURISTICS_MINER);
+        }
+
+        if(algorithms.size()>0) {
+            dailyRoutine = PredictionFramework.predict(PredictionFramework.retrieveTrainingData(mode), algorithms);
+        }else{
+            //TODO default prediction
+        }
     }
 
 
